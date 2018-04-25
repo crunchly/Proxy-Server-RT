3d50efb52485a96cf93676c979c2876bea6e44b9
rthai <rosarlin@rosarlins-MacBook-Pro.local>
2018-04-24 21:53:44 -0700

    displays all 4 modules

--- server.js
+++ server.js
@@ -1,11 +1,37 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const fetch = require('node-fetch');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client')));
+
+app.use('/funding-rounds', (req, res) => {
+  fetch(`http://localhost:3001${req.originalUrl}`)
+    .then(fetchRes => fetchRes.json())
+    .then(data => res.send(data));
+});
+
+app.use('/overview', (req, res) => {
+  fetch(`http://localhost:3002${req.originalUrl}`)
+    .then(response => response.json())
+    .then(data => res.send(data));
+});
+
+app.use('/people', (req, res) => {
+  fetch(`http://localhost:3004${req.originalUrl}`)
+    .then(response => response.json())
+    .then(data => res.send(data));
+});
+
+app.use('/api/funding_round', (req, res) => {
+  fetch(`http://localhost:3003${req.originalUrl}`)
+    .then(response => response.json())
+    .then(data => res.send(data));
+});
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
