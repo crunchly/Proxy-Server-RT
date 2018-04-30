38e5a3bf6523ec72534b6e6864627679748c7346
rthai <ahnamiko@gmail.com>
2018-04-30 00:27:28 -0700

    delete comment

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const fetch = require('node-fetch');
+const axios = require('axios')
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,23 +10,18 @@ const port = process.env.PORT || 3000;
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'client')));
 
+
 app.use('/funding-rounds', (req, res) => {
   fetch(`http://localhost:3001${req.originalUrl}`)
     .then(fetchRes => fetchRes.json())
     .then(data => res.send(data));
 });
 
-
 app.use('/overview', (req, res) => {
   fetch(`http://localhost:3002${req.originalUrl}`)
     .then(response => response.json())
     .then(data => res.send(data));
 });
-
-// app.get('http://localhost:3002/overview', (req, res) => {
-//   res.send()
-// });
-
 
 app.use('/people', (req, res) => {
   fetch(`http://localhost:3004${req.originalUrl}`)
